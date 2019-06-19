89d4d81ea1f2447c07ce4e3f7b42b75fc4b1e744
thepinkturtle <devinlax@hotmail.com>
2019-06-19 12:26:23 -0700

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,5 @@
-""" Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward. """
+""" Also, taken straight from leetcode.com
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward. """
 
 class Solution(object):
     def isPalindrome(self, x):
